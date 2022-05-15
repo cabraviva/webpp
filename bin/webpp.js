4e990c2b7e3c715d1e8002cfadfc4213d2f5104b
Green_Lab <greencoder001@outlook.com>
2022-05-15 17:48:13 +0200

    Added shebang

--- bin/webpp.js
+++ bin/webpp.js
@@ -1,2 +1,8 @@
+#! /usr/bin/env node
+
+const chalk = require('chalk')
+const fs = require('fs')
+const path = require('path')
+
 console.log('Welcome to webpp!')
 console.log('This package name is reserved for https://github.com/greencoder001/webpp')
